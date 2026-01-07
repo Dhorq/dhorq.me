8106b5ba391bd6e5cd1ad4ed6e0812a53815e906
Ridho Rifqy Muhammad <ridhorifqy34@gmail.com>
2026-01-07 14:49:10 +0700

    Add background for dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Navbar from "./components/Navbar";
+import useGlobalStore from "./stores/useGlobalStore";
 
 const App = () => {
+  const darkMode = useGlobalStore((state) => state.darkMode);
+
   return (
-    <div className="p-10">
+    <div
+      className={`${
+        darkMode ? "bg-gray-800 text-white" : "bg-white text-black"
+      } p-10 min-h-screen`}
+    >
       <Navbar />
     </div>
   );
