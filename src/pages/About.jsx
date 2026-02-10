7daf9c0c1776709d66bb8d1b362e4af2e8ca1042
Ridho Rifqy Muhammad <ridhorifqy34@gmail.com>
2026-02-10 23:12:21 +0700

    fix: fix about me section to display better ui

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <div className="flex justify-center items-center flex-col gap-10">
       <h1 className="text-2xl lg:text-3xl font-black">About</h1>
       <div className="flex text-justify flex-col gap-5 w-80 md:w-145 lg:w-150">
-        <p className="leading-8 indent-15">
+        <p className="leading-8">
           My name is <strong>Ridho Rifqy Muhammad</strong>, I am a recent
           Computer Science graduate from Bina Nusantara University and
           full-stack developer with experience building end-to-end web{" "}
@@ -12,7 +12,7 @@ const About = () => {
             Java.
           </i>
         </p>
-        <p className="leading-8 indent-15 mb-10">
+        <p className="leading-8 mb-10">
           I have a solid foundation in software engineering and application
           development, with experience in building{" "}
           <i className="text-green-400">
@@ -27,19 +27,19 @@ const About = () => {
           solutions for users.
         </p>
         <h4 className="font-bold">Academic Experiences</h4>
-        <p className="leading-8 indent-15">
+        <p className="leading-8">
           Since my fifth semester, I have focused on Data Analytics, gaining
           hands-on experience through internships as a System Analyst at the
           Ministry of Finance (KEMENKEU) and as a Data Analyst at the House of
           Representatives (DPR).
         </p>
-        <p className="leading-8 indent-15">
+        <p className="leading-8">
           After graduating, I developed a strong interest in Data Engineering
           and pursued it through self-directed learning to further strengthen my
           coding abilities and technical skills. I continued this learning
           journey until June.
         </p>
-        <p className="leading-8 indent-15">
+        <p className="leading-8">
           Since July, I have deliberately pursued a self-taught path as a Full
           Stack Developer, driven by my passion for problem-solving and building
           meaningful solutions. I have gained hands-on experience with{" "}
